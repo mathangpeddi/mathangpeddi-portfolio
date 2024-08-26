6b2503034dcb1d433d865876c098f9f02918e349
mathangpeddi <mathangpeddi@gmail.com>
2024-08-26 18:28:28 -0400

    Changed resume

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -61,7 +61,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Mathang Peddi.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = <h3 className="big-heading">Turning Complex Problems into Elegant Code.</h3>;
   const four = (
     <>
       <p>
